535eb91f7beccf7dc8ec13435b0cfe0ba05d77b8
kritsa0 <sao.pollakrit@gmail.com>
2026-05-13 18:14:19 +0700

    update: passwall url

--- passwall2x.sh
+++ passwall2x.sh
@@ -48,7 +48,7 @@ opkg update
 
 ### Add Src ###
 
-wget -O passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
+wget -O passwall.pub https://master.dl.sourceforge.net/project/openwrt-passwall-build/ipk.pub
 
 opkg-key add passwall.pub
 
